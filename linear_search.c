ecff8cc9d86c61c17bf72c57cf743c825aae563b
ujjwalgoyal65 <ujjwalgoyal65@gmail.com>
2019-07-13 10:08:30 +0530

    linearsearch

---
+++ linear_search.c
@@ -0,0 +1,19 @@
+#include <stdio.in>
+int main(){
+int n;
+scanf("%d",&n);
+int *arr=(int*)calloc(n,sizeof(int));
+for(int i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+int num;
+scanf("%d",&num);
+for(int i=0;i<n;i++){
+if(num==arr[i]){
+printf("index = %d",i);
+break;
+}
+}
+return 0;
+}
