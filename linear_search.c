2f7a45d893f87e36a461fb6fcb610b94adf25152
ujjwalgoyal65 <ujjwalgoyal65@gmail.com>
2019-08-02 10:00:17 +0530

    update linear

--- linear_search.c
+++ linear_search.c
@@ -2,13 +2,16 @@
 #include<stdlib.h>
 int main(){
 int n;
+printf("Enter the no. of elements");
 scanf("%d",&n);
 int *arr=(int*)calloc(n,sizeof(int));
+printf("enter the elements\n");
 for(int i=0;i<n;i++)
 {
 scanf("%d",&arr[i]);
 }
 int num;
+printf("enter no. u want search ");
 scanf("%d",&num);
 for(int i=0;i<n;i++){
 if(num==arr[i]){
