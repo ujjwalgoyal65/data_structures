944f7305e3ab959865e7a969694ad8e359a6f2ac
ujjwalgoyal65 <ujjwalgoyal65@gmail.com>
2019-08-02 09:58:33 +0530

    update gcd

--- gcd.c
+++ gcd.c
@@ -6,15 +6,20 @@ int main(int argc,char* argv[]){
 	for(int i=1;i<argc;i++){
 		arr[i-1]=atoi(argv[i]);
 	}
+	int g=gcd(arr,argc-1);
+	printf("gcd is %d",g);
+}
+int gcd(int arr[],int n){
+	int count=0,u=0;
 	for(int j=1;j<=arr[0];j++){
-		for(int k=0;k<argc-1;k++){
+		for(int k=0;k<n;k++){
 			if(arr[k]%j==0){
 				count++;
 			}
-		}if(count==argc-1){
+		}if(count==n){
 			u=j;
 		}
 		count=0;
 	}
-	printf("%d",u);
+	return u;
 }
